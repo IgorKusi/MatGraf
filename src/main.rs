cc625f565a17fce0ba56f43875860f6c50c1cb34
IgorKusi <igor.kusidel@gmail.com>
2023-10-14 22:22:08 +0200

    ADDED VERY IMPORTANT PRINTS {[(!!!HIGH PRIORITY HOTFIX PATCH!!!)]}

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod vector;
 use vector::Vector2;
 
 fn main() {
-    println!("Hello, world!");
     let v1 = Vector2::new(21.0, 20.0);
     let v2 = Vector2::new(10.0, -10.0);
     let scalar: f64 = 10.0;
@@ -12,9 +11,12 @@ fn main() {
     let sub = v2.sub(&v1);
     let scalmar = v1.scal_mul(&scalar);
 
-    println!("SUM");
+
+    println!("Pierwszy wektor: {}", v1);
+    println!("Drugi wektor: {}", v2);
+    println!("\nSUM");
     println!("{}", sum);
-    println!("\nSUB");
+    println!("\nSUB (v2 - v1)");
     println!("{}", sub);
     println!("\nSCALAR MULTIPLIKEJSZYN");
     println!("{}", scalmar)
